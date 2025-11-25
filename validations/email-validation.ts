5f5d108ed775b09e77198abb4ffdc4366deb546d
Franco <frannvallone@gmail.com>
2025-11-25 00:37:17 -0300

    - Mejora en validación de YUP con emails (matches)

--- validations/email-validation.ts
+++ validations/email-validation.ts
@@ -3,7 +3,12 @@ import * as Yup from "yup";
 export const emailSchema = Yup.object().shape({
     email: Yup.string()
         .email("Ingresa un correo válido")
-        .required("El correo es obligatorio"),
+        .required("El correo es obligatorio")
+        .lowercase()
+        .trim()
+        .matches(/^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/,
+            "Ingresa un correo válido"
+        ),
 });
 
 export interface EmailFormValues {
